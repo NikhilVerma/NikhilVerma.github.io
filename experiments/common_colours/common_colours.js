da69993a5ba8fb65f1668459df525461e207f06f
Nikhil Verma <nikhil.verma@corp.badoo.com>
2013-12-09 14:39:37 +0000

    Update the script and styles

--- experiments/common_colours/common_colours.js
+++ experiments/common_colours/common_colours.js
@@ -1,28 +1,64 @@
-// Please don't judge my code quality. I hacked this together while experimenting.
-
+/**
+ * Utility methods
+ * @type {Object}
+ */
 var Utils = {
-    toYUV: _.memoize(function (r, g, b) {
-        var y = r * 0.299000 + g * 0.587000 + b * 0.114000;
-        var u = 0.713 * (r - y);
-        var v = 0.564 * (b - u);
-        return [y, u, v];
-    }, function (r, g, b) {
-        return '' + r + g + b;
-    }),
 
-    euclidianDistance: function (a, b) {
-        return Math.sqrt(Math.pow(a[0] - b[0], 2) * 0.1 + Math.pow(a[1] - b[1], 2) + Math.pow(a[2] - b[2], 2));
+    /**
+     * Converts RGB to YUV color.
+     * @param  {Number} r
+     * @param  {Number} g
+     * @param  {Number} b
+     * @return {Array}
+     */
+    toYUV: function (r, g, b) {
+        var y = r * 0.299000 + g * 0.587000 + b * 0.114000;
+        var u = 0.565 * (b - y);
+        var v = 0.713 * (r - y);
+        return [y, u, v];
     },
 
+    /**
+     * Converts YUV to RGB color.
+     * @param  {Number} r
+     * @param  {Number} g
+     * @param  {Number} b
+     * @return {Array}
+     */
+    toRGB: function (y, u, v) {
+        var r = Math.round(y + 1.40 * v);
+        var g = Math.round(y - 0.344 * u - 0.714 * v);
+        var b = Math.round(y + 1.770 * u);
+
+        return [r, g, b];
+    },
+
+    /**
+     * Euclidian distance between two color sets
+     * @param  {Array} a an array with 3 num children
+     * @param  {Array} b an array with 3 num children
+     * @return {Number}   distance between the two colors
+     */
+    euclidianDistance: function (a, b) {
+        return Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2) + Math.pow(a[2] - b[2], 2);
+    },
+
+    /**
+     * Returns an array containing the common colors from an array of colours. It uses euclidian distance
+     * to find out colors which are close to each other. While also giving some importance to the colors
+     * which are too different from one another.
+     * @param  {Array} array Array of colors
+     * @return {Array}       Array of common colors
+     */
     getProminentColors: function (array) {
         var samples = [];
         var len = array.length;
         var i, j, dist;
-        var avgDist = 0;
 
         // Populate the colors array
+        // 1 is the weight given to a color
         for (i = 0, len = array.length; i < len; i++) {
-            samples.push([array[i], 0]);
+            samples.push([array[i], 1]);
         }
 
         // Compare each element in the sample with the other to calculate the distance
@@ -30,24 +66,23 @@ var Utils = {
         for (i = 0; i < len; i++) {
             for (j = len; j > i; j--) {
                 if (samples[i] && samples[j]) {
-                    dist = this.euclidianDistance(this.toYUV.apply(this, samples[i][0]), this.toYUV.apply(this, samples[j][0]));
-                    avgDist = (dist + avgDist) / 2;
 
-                    // Colors are similar
-                    if (dist < 20) {
-                        samples[i][1]++;
-                        // Empty the other element as it's too similar
+                    dist = this.euclidianDistance(samples[i][0], samples[j][0]);
+
+                    // Colors are similar, eat and kill the other one adding it's weight
+                    if (dist < 500) {
+                        samples[i][1] += samples[j][1];
                         samples[j] = null;
-                    } else if (dist > 100) {
-                        // colors are too different, we need to give those priority as well
-                        samples[j][1] += 2;
+                    }
+                    // colors are too different, we need to give those priority as well
+                    else if (dist > 6000) {
+                        samples[j][1] += 1;
                     }
                 }
             }
         }
 
-        console.log(avgDist);
-
+        // Sort and sum the output
         var output = [];
         var sum = 0;
 
@@ -66,55 +101,83 @@ var Utils = {
             colors: output,
             sum: sum
         };
-    }
-};
+    },
 
-var CanvasView = (function () {
+    /**
+     * Returns pixels from a source image using canvas. The input image is scaled
+     * down to prevent huge processing times
+     * @return {Array} array of YUV colors
+     */
+    getPixelsFromSrc: (function () {
 
-    var canvas = document.createElement('canvas');
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        var cb = _.indentity;
 
-    return {
-        el: canvas,
+        /**
+         * We scale the image down to 20x width
+         * @type {Number}
+         */
+        var TARGET_WIDTH = 20;
 
-        // We scale the image down to 100px width
-        width: 100,
+        /**
+         * Number of steps to take when scaling the image
+         * @type {Number}
+         */
+        var SCALE_STEPS = 10;
 
-        context: canvas.getContext('2d'),
+        var img = new Image();
+        img.onload = function () {
+            var width = canvas.width = img.width;
+            var height = canvas.height = img.height;
+            var i, len;
+            var currW = width;
+            var currH = height;
+            var lastW, lastH;
+            var step = (width - TARGET_WIDTH) / SCALE_STEPS;
+            var ratio = width / height;
+            var arr = [];
 
-        getImagePixelsFromSrc: function (src, callback) {
-            var img = new Image();
+            ctx.drawImage(img, 0, 0, width, height);
 
-            img.onload = _.bind(function () {
-                var width = canvas.width = this.width;
-                var height = canvas.height = this.width * img.height / img.width | 0;
-                this.context.drawImage(img, 0, 0, width, height);
+            for (i = 1; i <= SCALE_STEPS; i++) {
+                lastW = width - i * step;
+                lastH = lastW / ratio;
+                ctx.drawImage(canvas, 0, 0, currW, currH, 0, 0, lastW, lastH);
+                currW = lastW;
+                currH = lastH;
+            }
 
-                var arr = [],
-                    data = this.context.getImageData(0, 0, width, height).data;
+            var data = ctx.getImageData(0, 0, currW, currH).data;
 
-                for (var i = 0, len = data.length; i < len; i += 4) {
-                    arr.push([data[i], data[i + 1], data[i + 2]]);
-                }
+            for (i = 0, len = data.length; i < len; i += 4) {
+                arr.push(Utils.toYUV(data[i], data[i + 1], data[i + 2]));
+            }
 
-                callback(arr);
-            }, this);
+            cb(arr);
+        };
 
+        return function (src, callback) {
+            cb = callback;
             img.src = src;
-        }
-    };
-})();
+        };
+    })()
+};
 
 var handleFileSelect = function (e) {
     var reader = new FileReader();
     var output = document.getElementById('output');
 
     reader.onload = function (e) {
-        CanvasView.getImagePixelsFromSrc(e.target.result, function (pixels) {
-            var colors = Utils.getProminentColors(pixels);
+        Utils.getPixelsFromSrc(e.target.result, function (pixels) {
 
+            var colors = Utils.getProminentColors(pixels);
+            var i, len, width;
             var str = '';
+
             for (i = 0, len = colors.colors.length; i < len; i++) {
-                str += '<span style="background-color:rgb(' + colors.colors[i][0] + '); width:' + (colors.colors[i][1] / colors.sum * 100) + '%">' + Math.round(colors.colors[i][1] / colors.sum * 100) + '%</span>';
+                width = colors.colors[i][1] / colors.sum * 100;
+                str += '<span style="background-color:rgb(' + Utils.toRGB.apply(this, colors.colors[i][0]) + '); width:' + width + '%"><strong>' + Math.round(width) + '%</strong></span>';
             }
 
             output.innerHTML = str;
